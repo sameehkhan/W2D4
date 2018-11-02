3ff9c9296f3028377c10b6101e5d139c752be7d9
sameehkhan <sameehkhan1@gmail.com>
2018-11-01 17:28:49 -0700

    Make O(n) time anagram method

--- Big_O/anagrams.rb
+++ Big_O/anagrams.rb
@@ -1,4 +1,4 @@
-
+  require 'byebug'
 
   def first_anagram?(str1, str2)
     letters = str1.split("")
@@ -9,8 +9,33 @@
   
   def second_anagram?(str1,str2)
     
+    new_arr = str2.split("")
+    
+    return false if str1.length != str2.length
+    
     str1.each_char.with_index do |ch, i|
-      str2.delete!(ch) if str2.index(ch) != nil 
+      idx2 = new_arr.index(ch)
+      new_arr.delete_at(idx2) if idx2 != nil 
     end 
-     str2 == ""
+     new_arr.empty?
+  end 
+  
+  
+
+  
+  def third_anagram?(str1,str2)
+    str1.split("").sort == str2.split("").sort
+  end 
+  
+  
+  def fourth_anagram?(str1,str2)
+    hash = Hash.new(0)
+    
+    return false if str1.length != str2.length
+    str1.each_char.with_index do |el, i|
+      hash[str1[i]] += 1
+      hash[str2[i]] -= 1
+    end 
+    
+    hash.values.all? {|ch| ch.zero? }
   end 
